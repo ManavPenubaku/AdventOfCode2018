21f5e8b949c94fc4bd6e92fea132a76686aa2b09
Manav Penubaku <mpenubaku@rbionics.com>
2022-01-09 11:18:37 +0100

    solved part 2 of day 1 with hashset in rust

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,4 +1,5 @@
 use aoc2018::util;
+use std::collections::HashSet;
 
 fn read_ints(path: &str) -> Vec<i32>{
     let mut out: Vec<i32> = Vec::new();
@@ -12,9 +13,23 @@ fn read_ints(path: &str) -> Vec<i32>{
 }
 fn main() {
     let nums = read_ints("inputs/day1.txt");
-    let mut frequency = 0;
+    let mut frequency_p1 = 0;
     for num in nums.iter(){
-        frequency += num;
+        frequency_p1 += num;
     }
-    println!("Solution to Part 1 is : {}",frequency);
+    println!("Solution to Part 1 is : {}",frequency_p1);
+
+    let mut frequency_dict: HashSet<i32> = HashSet::new();
+    let mut frequency_p2 = 0;
+    let mut index = 0;
+    while !frequency_dict.contains(&frequency_p2){
+        frequency_dict.insert(frequency_p2);
+        frequency_p2 += nums[index];
+        if index < nums.len() - 1{
+            index += 1;
+        }else {
+            index = 0;
+        }
+    }
+    println!("Solution to Part 2 is : {}",frequency_p2)
 }
